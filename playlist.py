42dae8f91219731d1650dfc8e0735bfebdf1ea66
Mike Matera <matera@lifealgorithmic.com>
2017-01-15 10:23:12 -0800

    Refactor and some cleanup.

--- playlist.py
+++ playlist.py
@@ -20,7 +20,83 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainPage(webapp2.RequestHandler):    
+class MixList(webapp2.RequestHandler):    
+
+    def check_metadata(self, blob):
+        update_md = False
+        md = blob.metadata
+        if md is None :
+            md = {}
+
+        # Check if the ID3 tags have been scanned.            
+        if 'id3.analyzed' not in md :
+            update_md = True
+            blobfilename = '/' + blob.bucket.name + '/' + blob.name
+            logging.info("Attempting to analyze " + blobfilename)
+            filelike = gcs.open(blobfilename)
+            try :
+                tagfile = EasyID3(filelike)
+                # Copy all tags.
+                for tag in tagfile :
+                    md['id3.' + tag] = tagfile[tag][0]
+                md['id3.analyzed'] = 'ok'
+
+            except Exception as e :
+                logging.error("Error analyzing " + blob.name + ": " + e)
+                md['id3.analyzed'] = 'error'
+
+            filelike.close()
+
+        # Verify that all metadata is intact. 
+        if 'Title' not in md :
+            update_md = True
+            if 'id3.title' in md :
+                md['Title'] = md['id3.title']
+            else :
+                md['Title'] = blob.name
+
+        if 'Comment' not in md : 
+            update_md = True
+            if 'id3.comment' in md :
+                md['Comment'] = md['id3.comment']
+            else :
+                md['Comment'] = ''
+
+        if 'Image' not in md :
+            update_md = True
+            if 'id3.image' in md :
+                md['Image'] = md['id3.image']
+            else :
+                md['Image'] = 'http://www.w3schools.com/w3images/lights.jpg'
+
+        if update_md :           
+            blob.metadata = md
+            blob.patch()
+        
+        return md
+    
+    def cache_image(self, bucket, image_url):
+        image_name = "cached_images/image-%08x" % binascii.crc32(image_url)
+
+        # Try to find the cached image in my bucket
+        cached_image = bucket.get_blob(image_name)
+        if cached_image is None :
+            # Cache miss: Retrieve the URL and store the image
+            try:
+                result = urlfetch.fetch(image_url)
+                if result.status_code == 200:
+                    cached_image = bucket.blob(image_name)
+                    cached_image.upload_from_string(result.content, result.headers['content-type'])
+                    cached_image.make_public()
+                else:
+                    logging.error("Unable to fetch image URL " + image_url + " HTTP status: " + str(result.status_code))
+                    return image_url 
+                
+            except urlfetch.Error as e:
+                    logging.error("Unable to fetch image URL " + image_url + " Caught: " + repr(e))
+                    return image_url
+        
+        return cached_image.public_url   
     
     def get(self):
         
@@ -39,82 +115,22 @@ class MainPage(webapp2.RequestHandler):
             if blob.content_type == 'audio/mp3' :
 
                 # Check for the presence of id3 info in the metadata, generate it.
-                md = blob.metadata
-                if md is None :
-                    md = {}
+                md = self.check_metadata(blob)
 
-                if 'id3.analyzed' not in md :
-                    logging.info("Attempting to analyze " + blob.name)
-                    filelike = gcs.open('/' + bucket.name + '/' + blob.name)
-                    try :
-                        tagfile = EasyID3(filelike)
-                        # Copy all tags.
-                        for tag in tagfile :
-                            md['id3.' + tag] = tagfile[tag][0]
-
-                        # Check for the tags I'm interested in.
-                        if 'title' in tagfile :
-                            md['Title'] = tagfile['title'][0]
-                        else :
-                            md['Title'] = blob.name
-
-                        if 'comment' in tagfile :
-                            md['Comment'] = tagfile['comment'][0]
-                        else :
-                            md['Comment'] = ''
-
-                        md['Image'] = 'http://www.w3schools.com/w3images/lights.jpg'
-
-                        md['id3.analyzed'] = 'ok'
-                    except Exception as e :
-                        logging.error("Error analyzing " + blob.name + ": " + e)
-                    
-                    filelike.close()
-                    blob.metadata = md
-                    blob.patch()
-
-                # Check for the image in metadata.
-                if 'Image' in md :
-                    base_image_url = md['Image']
-                else :
-                    base_image_url = 'http://www.w3schools.com/w3images/lights.jpg'
-
-                # Check the title
-                if 'Title' in md :
-                    mix_title = md['Title']
-                else :
-                    mix_title = blob.name
-
-                image_name = "cached_images/image-%08x" % binascii.crc32(base_image_url)
-                image_url = base_image_url
-
-                # Try to find the cached image in my bucket
-                cached_image = bucket.get_blob(image_name)
-                if cached_image is None :
-                    # Retrieve the URL and store the image
-                    try:
-                        result = urlfetch.fetch(base_image_url)
-                        if result.status_code == 200:
-                            cached_image = bucket.blob(image_name)
-                            cached_image.upload_from_string(result.content, result.headers['content-type'])
-                            cached_image.make_public()
-                            image_url = cached_image.public_url
-                        else:
-                            logging.error("Unable to fetch image URL " + image_url + " HTTP status: " + str(result.status_code))
-                            pass
-                    except urlfetch.Error as e:
-                            logging.error("Unable to fetch image URL " + image_url + " Caught: " + repr(e))
+                # Check the image cache.
+                image_url = self.cache_image(bucket, md['Image'])
 
                 template_values['mixes'].append({
-                        'name' : mix_title,
-                        'url' : blob.public_url,
-                        'img' : image_url
+                        'name'    : md['Title'],
+                        'comment' : md['Comment'],
+                        'url'     : blob.public_url,
+                        'img'     : image_url
                     })
         
         template = JINJA_ENVIRONMENT.get_template('playlist.html')
         self.response.write(template.render(template_values))
                    
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+application = webapp.WSGIApplication([('/', MixList)], debug=True)
 
 def main():
     run_wsgi_app(application)
